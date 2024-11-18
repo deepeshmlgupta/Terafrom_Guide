e1037f48808f866bf6779c170bab366db7f823a1
Ubuntu <ubuntu@ip-172-31-44-66.ap-south-1.compute.internal>
2024-11-18 07:17:54 +0000

    Added a multi resource file

---
+++ Day1/multiresource.tf
@@ -0,0 +1,21 @@
+#this resource will add a txt file named demo.txt
+resource "local_file" "textfile" {
+        filename = "/home/ubuntu/Day3/demo.txt"
+        content = "This the example for multi0-resource"
+}
+
+
+#this file will create a random string
+resource "random_string" "rand-str" {
+        length = 20
+        special = true
+        override_special = "!@#"
+}
+
+output "rand-str" {
+        value = random_string.rand-str[*].result
+}
+
+
+#this purpose of creating this file that we can add multiple resources in a single terraform file
+
